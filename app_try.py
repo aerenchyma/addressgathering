934392f9d98705f3ff4cdb581f91551f63847a1d
aerenchyma <jczetta@gmail.com>
2012-11-30 23:36:00 -0500

    works for two goes of next_page, now to alter for all

---
+++ app_try.py
@@ -0,0 +1,10 @@
+# one: create spreadsheet with ____ within max. radius of one of those addresses with corret info and save it
+# two: procedure to add to, not overwrite, spreadsheet with range of all those addresses, same key, for one entering of the key
+# three: remove duplicates from spreadsheet and check 
+
+#LATER:
+## app (flask + heroku?)
+## options for information..
+
+########
+
