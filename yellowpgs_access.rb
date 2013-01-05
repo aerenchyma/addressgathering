ebc7b9e3d0390f9631fedc01096c1a76771d62fa
aerenchyma <jczetta@gmail.com>
2013-01-05 16:58:51 -0500

    yellowpages access works for cmdline script, creates csv

--- yellowpgs_access.rb
+++ yellowpgs_access.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'mechanize'
 require 'nokogiri'
 require 'open-uri'
+require 'cgi'
 
 $agent = Mechanize.new
 page = $agent.get('http://yellowpages.com')
@@ -13,55 +14,18 @@ searchform.search_terms = "Veternary Services"
 searchform.geo_location_terms = "Detroit, MI"
 results = $agent.submit(searchform)
 start_pg = results.uri # this is the page to start scraping from
+pg = Nokogiri::HTML(open(start_pg)) # Nokogiri document of the start page
 
-## name, after <h3 class="business-name fn org" + bunch of other stuff before end tag
-## address, within <span class="street-address">, below the former mess
-## and then there is:
-
-## <span class="city-state"><span class="locality">Detroit</span>,
-##<span class="region">MI</span>
-##<span class="postal-code">48227</span>
-##</span>
-##</span>
-
-## for each
-
-## then to go to the next page,
-
-## original url is something like
-## http://www.yellowpages.com/detroit-mi/places-of-worship?g=Detroit%2C+MI&q=places+of+worship
-## next page url adds param as such:
-## http://www.yellowpages.com/detroit-mi/places-of-worship?g=Detroit%2C+MI&page=2&q=places+of+worship
-
-pg = Nokogiri::HTML(open(start_pg))
-#p pg.css("span[class='locality']").text #City names (all)
-#p pg.css("span[class='region']").text # State abbrv (all)
-#p pg.css("span[class='postal-code']").text # zip code (all)
-#p pg.css("h3[class='business-name fn org']").text.gsub!(/\s+/, "") #names without spaces (all) -- but note that this is not an iterable..
-
-#citynames = pg.css("span").select{|nm| nm["class"] == "locality"}
-#citynames.each{|nm| puts nm.text}
 
 $place_names = pg.css("h3").select{|xc| xc["class"] == "business-name fn org"}
 $addresses = pg.css("span.street-address")
 $citynames = pg.css("span.locality")
 $statenames = pg.css("span.region")
 $zipcodes = pg.css("span.postal-code")
-#citynames.each {|nm| puts nm.text}
 
-# so this works. want to collect all the information into an array doc each time
-
-places_info = []
-
-#h3_string = "business-name fn org"
-#place_names = pg.css("h3").select{|xc| xc["class"] == "business-name fn org"}
-
-#place_names.each {|x| puts x.text.strip!}
 
 $baseurl = "http://yellowpages.com"
 $doc_hashes = []
-#while pg.css("li.next")
-  
   
 def create_hashes(page)
   $place_names = page.css("h3").select{|xc| xc["class"] == "business-name fn org"}
@@ -74,7 +38,7 @@ def create_hashes(page)
     a = Hash.new
     begin
     a['name'] = $place_names[count].text.strip! 
-    a['addr'] = $addresses[count].text.strip! # this is where it goes nil when there's nothing
+    a['addr'] = $addresses[count].text.strip!.gsub!(",","") # this is where it goes nil when there's nothing
     a['city'] = $citynames[count].text
     a['state'] = $statenames[count].text
     a['zip'] = $zipcodes[count].text
@@ -84,34 +48,42 @@ def create_hashes(page)
   else
     count += 1
     $doc_hashes << a
-    pp a
+    #pp a
   end
   end
-  #p $doc_hashes
+  #pp $doc_hashes
   if page.css("li.next")
     sel = page.css("li.next a").map {|link| link['href']}
-    #pp sel
     n_url = sel[0]
     if n_url
       n_pg = Nokogiri::HTML(open($baseurl + n_url))
       create_hashes(n_pg)
     end
   end
-  #p $doc_hashes
+  $doc_hashes
 end
 
-create_hashes(pg)
+def transform_hash(hn)
+  s = "#{hn['name']}, #{hn['addr']}, #{hn['city']}, #{hn['state']}, #{hn['zip']}\n"
+  s
+end
+
+$fname = "test_1.csv"
+csv_header = %w{Name Address City State Zip}.map {|w| CGI.escape(w) }.join(", ") + "\n"
+
+hashed_infos = create_hashes(pg)
+
+f = File.open($fname, 'w') 
+f.write(csv_header)
+
+hashed_infos.each do |h|
+  f.write(transform_hash(h))
+end
+
+f.close
 
 # YAY
 
 ## create CSVs -- how to handle?
 ## in CSVs -- want to remove duplicate addresses (yes?)
-
-
-#test_stuff.each do |yz|
-#  c_name = test_stuff.css("span.locality")
-#  s_name = test_stuff.css("span.region")
-#  c_name.each {|x| p x.text}
-#  s_name.each {|y| p y.text}
-#end
 
